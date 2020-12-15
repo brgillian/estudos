c6040d9381509f0ad91ae1b9d2da551f15be8c3c
brgillian <brgillian@gmail.com>
2020-12-15 09:17:02 -0300

    Exercicio de entrada de dados 2

---
+++ java-completo-nelio-alves/S4A26EntradaDados.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class S4A26EntradaDados {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s1, s2, s3;
+        s1 = sc.nextLine();
+        
+    }
+}
